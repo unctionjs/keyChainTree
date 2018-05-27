14c7e1f04dbfad541771c5a5e954a0907808b061
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-27 00:23:57 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
-import get from "@unction/get"
-import reduceValues from "@unction/reducevalues"
-import isType from "@unction/istype"
-import mapValues from "@unction/mapvalues"
-import prepend from "@unction/prepend"
-import append from "@unction/append"
-import getMany from "@unction/getmany"
-import mergeRight from "@unction/mergeright"
+import get from "@unction/get";
+import reduceValues from "@unction/reducevalues";
+import isType from "@unction/istype";
+import mapValues from "@unction/mapvalues";
+import prepend from "@unction/prepend";
+import append from "@unction/append";
+import getMany from "@unction/getmany";
+import mergeRight from "@unction/mergeright";
+export default function keyChainTree(tree) {
+  return reduceValues(accumulated => key => {
+    const value = get(key)(tree);
 
-import type {RecordTreeType} from "types"
-import type {KeyChainType} from "types"
-import type {UnaryFunctionType} from "types"
-import type {ArrayType} from "types"
+    if (isType("Object")(value) || isType("Map")(value)) {
+      return mergeRight(accumulated)(mapValues(prepend(key))(keyChainTree(value)));
+    }
 
-export default function keyChainTree (tree: RecordTreeType): ArrayType<KeyChainType> {
-  return reduceValues(
-    (accumulated: ArrayType<KeyChainType>): UnaryFunctionType<*, *> =>
-      (key: mixed): ArrayType<KeyChainType> => {
-        const value: mixed = get(key)(tree)
-
-        if (isType("Object")(value) || isType("Map")(value)) {
-          return mergeRight(accumulated)(mapValues(prepend(key))(keyChainTree(value)))
-        }
-
-        return append([key])(accumulated)
-      }
-  )(
-    []
-  )(
-    getMany(tree)
-  )
+    return append([key])(accumulated);
+  })([])(getMany(tree));
 }
